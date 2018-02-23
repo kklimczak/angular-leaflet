318ce430e615f51facbfcf1665f1466558ea1b30
kklimczak <konrad.klimczak.4@gmail.com>
2018-02-23 20:21:23 +0100

    add lib module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {LibModule} from './lib/lib.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
