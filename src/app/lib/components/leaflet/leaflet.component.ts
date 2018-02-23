6e45d4bc332ad64131e9ad8943b5ed7f5915eb06
kklimczak <konrad.klimczak.4@gmail.com>
2018-02-23 20:25:32 +0100

    init leaflet component

---
+++ src/app/lib/components/leaflet/leaflet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leaflet',
+  templateUrl: './leaflet.component.html',
+  styleUrls: ['./leaflet.component.scss']
+})
+export class LeafletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/lib/lib.module.ts
+++ src/app/lib/lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LeafletComponent } from './components/leaflet/leaflet.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [LeafletComponent],
+  exports: [LeafletComponent]
 })
 export class LibModule { }
