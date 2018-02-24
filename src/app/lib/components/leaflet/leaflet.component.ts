fd4b56d2648db7c47ce45d97f0dd25c4fb5c36c7
kklimczak <konrad.klimczak.4@gmail.com>
2018-02-24 12:52:28 +0100

    display map container

--- src/app/lib/components/leaflet/leaflet.component.ts
+++ src/app/lib/components/leaflet/leaflet.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
+import {map, Map} from 'leaflet';
 
 @Component({
   selector: 'app-leaflet',
   templateUrl: './leaflet.component.html',
   styleUrls: ['./leaflet.component.scss']
 })
-export class LeafletComponent implements OnInit {
+export class LeafletComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  map: Map;
+
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.map = map(this.elementRef.nativeElement, {});
   }
 
 }
