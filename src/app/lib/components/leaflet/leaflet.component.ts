4b87aed79490440b004bad03b5c4a9d3a67dbc99
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-01 23:16:45 +0100

    add feature group to map

--- src/app/lib/components/leaflet/leaflet.component.ts
+++ src/app/lib/components/leaflet/leaflet.component.ts
@@ -1,6 +1,7 @@
 import {AfterContentInit, AfterViewInit, Component, ContentChildren, ElementRef, Input, OnInit, QueryList} from '@angular/core';
 import {map, Map, MapOptions} from 'leaflet';
 import {BaseLayer} from '../base-layer';
+import {FeatureGroupComponent} from '../feature-group/feature-group.component';
 
 @Component({
   selector: 'app-leaflet',
@@ -12,6 +13,7 @@ export class LeafletComponent implements OnInit, AfterContentInit {
   @Input() options: MapOptions;
 
   @ContentChildren(BaseLayer) layers: QueryList<BaseLayer>;
+  @ContentChildren(FeatureGroupComponent) groups: QueryList<FeatureGroupComponent>;
 
   map: Map;
 
@@ -27,6 +29,10 @@ export class LeafletComponent implements OnInit, AfterContentInit {
     this.layers.forEach(layer => layer.addTo(this.map));
     this.layers.changes
       .subscribe(() => this.layers.forEach(layer => layer.addTo(this.map)));
+
+    this.groups.forEach(group => group.addTo(this.map));
+    this.groups.changes
+      .subscribe(() => this.groups.forEach(group => group.addTo(this.map)));
   }
 
 }
--- src/app/lib/lib.module.ts
+++ src/app/lib/lib.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeafletComponent } from './components/leaflet/leaflet.component';
 import { TileLayerComponent } from './components/tile-layer/tile-layer.component';
+import { FeatureGroupComponent } from './components/feature-group/feature-group.component';
+import { MarkerComponent } from './components/marker/marker.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LeafletComponent, TileLayerComponent],
-  exports: [LeafletComponent, TileLayerComponent]
+  declarations: [LeafletComponent, TileLayerComponent, FeatureGroupComponent, MarkerComponent],
+  exports: [LeafletComponent, TileLayerComponent, FeatureGroupComponent, MarkerComponent]
 })
 export class LibModule { }
