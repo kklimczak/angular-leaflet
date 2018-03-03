17b80d2048daa629dfc9b29f9a0eef6a125202ce
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-03 18:01:22 +0100

    add routes to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import {LibModule} from './lib/lib.module';
 import {MatButtonModule, MatGridListModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Route, RouterModule} from '@angular/router';
+import {TilesComponent} from './features/tiles/tiles.component';
+import {TilesModule} from './features/tiles/tiles.module';
+import {ImageModule} from './features/image/image.module';
+import {ImageComponent} from './features/image/image.component';
 
+const routes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'tile-layer'
+  },
+  {
+    path: 'tile-layer',
+    component: TilesComponent
+  },
+  {
+    path: 'image-overlay',
+    component: ImageComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +39,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatButtonModule,
     MatListModule,
-    MatGridListModule
+    MatGridListModule,
+    TilesModule,
+    ImageModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
