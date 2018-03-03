e0c9d548430bf64b8db0dbfcb947096cc8896c05
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-03 14:12:15 +0100

    add function to prepare marker layer

--- src/app/lib/components/marker/marker.component.ts
+++ src/app/lib/components/marker/marker.component.ts
@@ -1,6 +1,6 @@
-import {Component, forwardRef, OnDestroy} from '@angular/core';
+import {Component, forwardRef, Input, OnDestroy} from '@angular/core';
 import {BaseLayer} from '../base-layer';
-import {divIcon, LayerGroup, Map, marker, Marker} from 'leaflet';
+import {divIcon, LatLngExpression, LayerGroup, Map, marker, Marker} from 'leaflet';
 
 @Component({
   selector: 'app-marker',
@@ -10,17 +10,38 @@ import {divIcon, LayerGroup, Map, marker, Marker} from 'leaflet';
 })
 export class MarkerComponent implements BaseLayer, OnDestroy {
 
+  @Input() set latLng(value: LatLngExpression) {
+    this._latLng = value;
+    this.prepareLayer();
+  }
+
   map: Map | LayerGroup;
   layer: Marker;
 
+  _latLng: LatLngExpression;
+
   constructor() { }
 
-  addTo(map: Map | LayerGroup): void {
-    this.map = map;
-    this.layer = marker([51.759445, 19.457216], {
+  private prepareLayer() {
+    if (this.map && this._latLng) {
+      if (this.layer) {
+        this.layer.setLatLng(this._latLng);
+      } else {
+        this.createLayer();
+      }
+    }
+  }
+
+  private createLayer() {
+    this.layer = marker(this._latLng, {
       icon: divIcon()
     });
     this.map.addLayer(this.layer);
+  }
+
+  addTo(map: Map | LayerGroup): void {
+    this.map = map;
+    this.prepareLayer();
   }
 
   removeFrom() {
