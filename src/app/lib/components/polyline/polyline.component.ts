d198bcc658a263921b397c736347ab6d7defbac5
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-04 14:08:40 +0100

    add polyline layer component

---
+++ src/app/lib/components/polyline/polyline.component.ts
@@ -0,0 +1,63 @@
+import {Component, forwardRef, Input, OnDestroy} from '@angular/core';
+import {BaseLayer} from '../base-layer';
+import {LatLngExpression, LayerGroup, Map, PathOptions, polyline, Polyline} from 'leaflet';
+
+@Component({
+  selector: 'app-polyline',
+  templateUrl: './polyline.component.html',
+  styleUrls: ['./polyline.component.scss'],
+  providers: [{provide: BaseLayer, useExisting: forwardRef(() => PolylineComponent)}]
+})
+export class PolylineComponent implements BaseLayer, OnDestroy {
+
+  @Input() set coordinates(value: LatLngExpression[]) {
+    this._coordinates = value;
+    if (this.layer) {
+      this.layer.setLatLngs(value);
+    } else {
+      this.createLayer();
+    }
+  }
+
+  @Input() set options(value: PathOptions) {
+    this._options = value;
+    if (this.layer) {
+      this.layer.setStyle(value);
+    } else {
+      this.createLayer();
+    }
+  }
+
+  map: Map | LayerGroup;
+  layer: Polyline;
+
+  private _coordinates: LatLngExpression[];
+  private _options: PathOptions;
+
+  constructor() { }
+
+  addTo(map: Map | LayerGroup): void {
+    this.map = map;
+    if (!this.layer && this._coordinates) {
+      this.createLayer();
+    }
+  }
+
+  private createLayer() {
+    if (this._coordinates) {
+      this.layer = polyline(this._coordinates, {
+        ...this._options
+      });
+      this.map.addLayer(this.layer);
+    }
+  }
+
+  removeFrom(): void {
+    (this.map as any).removeLayer(this.layer);
+  }
+
+  ngOnDestroy() {
+    this.removeFrom();
+  }
+
+}
--- src/app/lib/lib.module.ts
+++ src/app/lib/lib.module.ts
@@ -5,12 +5,13 @@ import { TileLayerComponent } from './components/tile-layer/tile-layer.component
 import { ImageOverlayComponent } from './components/image-overlay/image-overlay.component';
 import {FeatureGroupComponent} from './components/feature-group/feature-group.component';
 import {MarkerComponent} from './components/marker/marker.component';
+import { PolylineComponent } from './components/polyline/polyline.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LeafletComponent, TileLayerComponent, FeatureGroupComponent, MarkerComponent, ImageOverlayComponent],
-  exports: [LeafletComponent, TileLayerComponent, FeatureGroupComponent, MarkerComponent, ImageOverlayComponent]
+  declarations: [LeafletComponent, TileLayerComponent, FeatureGroupComponent, MarkerComponent, ImageOverlayComponent, PolylineComponent],
+  exports: [LeafletComponent, TileLayerComponent, FeatureGroupComponent, MarkerComponent, ImageOverlayComponent, PolylineComponent]
 })
 export class LibModule { }
