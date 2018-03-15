7b410336f21f2107f1cfabd7ceed001e6ceb6051
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-15 19:59:16 +0100

    aggregate all modules in LeafletModule

--- src/leaflet.module.ts
+++ src/leaflet.module.ts
@@ -1,11 +1,27 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {CoreModule, ImageOverlayModule, MarkerModule, TileLayerModule, VectorsModule} from 'angular-leaflet';
+import {GroupsModule} from './groups';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule,
+    GroupsModule,
+    ImageOverlayModule,
+    MarkerModule,
+    TileLayerModule,
+    VectorsModule
   ],
   declarations: [],
-  exports: []
+  exports: [
+    CommonModule,
+    CoreModule,
+    GroupsModule,
+    ImageOverlayModule,
+    MarkerModule,
+    TileLayerModule,
+    VectorsModule
+  ]
 })
 export class LeafletModule { }
--- src/public_api.ts
+++ src/public_api.ts
@@ -1,5 +1,4 @@
 export {LeafletModule} from './leaflet.module';
-export {BaseLayer} from './components/base-layer';
 export * from './core/index';
 export * from './tile-layer/index';
 export * from './image-overlay/index';
