bfd536cbe17acca91cf21e16579730c068bdf8a5
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-15 19:49:42 +0100

    move groups to external module

---
+++ src/groups/index.ts
@@ -0,0 +1,1 @@
+export {GroupsModule} from './groups.module';
--- src/leaflet.module.ts
+++ src/leaflet.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FeatureGroupComponent} from './components/feature-group/feature-group.component';
+import {FeatureGroupComponent} from './groups/feature-group/feature-group.component';
 import {MarkerComponent} from './components/marker/marker.component';
 
 @NgModule({
