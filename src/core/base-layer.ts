21ac39d935c6c643341d22f96276b3d3da5d7bd4
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-29 20:11:14 +0200

    convert base layer to normal class and implement repeatable methods in components

--- src/core/base-layer.ts
+++ src/core/base-layer.ts
@@ -1,16 +1,27 @@
 import {Layer, LayerGroup, Map} from 'leaflet';
-import {ContentChildren, QueryList} from '@angular/core';
+import {ContentChildren, OnDestroy, QueryList} from '@angular/core';
 import {LayerHandler} from './layer-handler';
 
-export class BaseLayer {
+export class BaseLayer implements OnDestroy {
   map: Map | LayerGroup;
   layer: Layer;
   @ContentChildren(LayerHandler) handlers: QueryList<LayerHandler>;
 
-  addTo(map: Map | LayerGroup): void {}
-  removeFrom(): void {}
+  addTo(map: Map | LayerGroup): void {
+    this.map = map;
+  }
+
+  removeFrom(): void {
+    if (this.layer) {
+      (this.map as any).removeLayer(this.layer);
+    }
+  }
 
   initHandlers() {
     this.handlers.forEach(handler => handler.initialize(this.layer['_map'], this.layer));
+  }
+
+  ngOnDestroy() {
+    this.removeFrom();
   }
 }
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
 export {BaseLayer} from './base-layer';
 export {MapHandler} from './map-handler';
+export {LayerHandler} from './layer-handler';
 export * from './core.module';
