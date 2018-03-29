5d19a3a0a1e2a4ed60243eb4e34b0384735e4406
kklimczak <konrad.klimczak.4@gmail.com>
2018-03-29 22:10:38 +0200

    add center param to circle layer

--- src/vectors/circle/circle.component.ts
+++ src/vectors/circle/circle.component.ts
@@ -1,6 +1,6 @@
-import {Component, forwardRef, OnInit} from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
 import {BaseLayer} from '../../core/base-layer';
-import {circle, Circle, LayerGroup, Map} from 'leaflet';
+import {circle, Circle, LatLngExpression, LayerGroup, Map} from 'leaflet';
 
 @Component({
   selector: 'app-circle',
@@ -10,7 +10,18 @@ import {circle, Circle, LayerGroup, Map} from 'leaflet';
 })
 export class CircleComponent extends BaseLayer {
 
+  @Input() set center(value: LatLngExpression) {
+    this._center = value;
+    if (this.layer) {
+      this.layer.setLatLng(value);
+    } else {
+      this.createLayer();
+    }
+  }
+
   layer: Circle;
+
+  private _center: LatLngExpression;
 
   constructor() {
     super();
@@ -19,7 +30,13 @@ export class CircleComponent extends BaseLayer {
   addTo(map: Map | LayerGroup): void {
     super.addTo(map);
     if (!this.layer) {
-      this.layer = circle([51.759445, 19.457216], {radius: 1000});
+      this.createLayer();
+    }
+  }
+
+  private createLayer() {
+    if (this.map && this._center) {
+      this.layer = circle(this._center, {radius: 1000});
       this.map.addLayer(this.layer);
       this.initHandlers();
     }
