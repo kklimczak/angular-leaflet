f92b23dd12724812f9538fb9b8da57b91001e62e
kklimczak <konrad.klimczak.4@gmail.com>
2018-07-09 22:32:40 +0200

    test(image-overlay): add missing tests

--- lib/src/image-overlay/image-overlay.component.spec.ts
+++ lib/src/image-overlay/image-overlay.component.spec.ts
@@ -1,6 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ImageOverlayComponent } from './image-overlay.component';
+import {ImageOverlayComponent} from './image-overlay.component';
+import {CRS, latLngBounds, Map} from 'leaflet';
+
+const url = 'http://localhost:4200/test.jpg';
+const width = 200;
+const height = 300;
 
 describe('ImageOverlayComponent', () => {
   let component: ImageOverlayComponent;
@@ -17,7 +22,82 @@ describe('ImageOverlayComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test('should prepare image layer', () => {
+    component.map = new Map(document.createElement('div'));
+    component.map.addLayer = jest.fn();
+    component.src = url;
+    component.height = height;
+    component.width = width;
+
+    fixture.detectChanges();
+
+    expect(component.layer).toBeTruthy();
+    expect(component.map.addLayer).toHaveBeenCalled();
+    expect(component.map.addLayer).toHaveBeenCalledWith(component.layer);
+  });
+
+  test('should prevent to prepare layer when map is missing', () => {
+    component.src = url;
+    component.height = height;
+    component.width = width;
+
+    fixture.detectChanges();
+
+    expect(component.layer).toBeFalsy();
+  });
+
+  test('should change width when input has been changed', () => {
+    setBaseComponentSetup(component);
+    fixture.detectChanges();
+
+    component.layer.setBounds = jest.fn();
+
+    component.width = 300;
+
+    fixture.detectChanges();
+
+    expect(component.layer.setBounds).toHaveBeenCalled();
+    expect(component.layer.setBounds).toHaveBeenCalledWith(latLngBounds([[-0, 300], [-300, 0]]));
+  });
+
+  test('should change height when input has been changed', () => {
+    setBaseComponentSetup(component);
+    fixture.detectChanges();
+
+    component.layer.setBounds = jest.fn();
+
+    component.height = 200;
+    fixture.detectChanges();
+
+    expect(component.layer.setBounds).toHaveBeenCalled();
+    expect(component.layer.setBounds).toHaveBeenCalledWith(latLngBounds([[-0, 200], [-200, 0]]));
+  });
+
+  test('should change url when input has been changed', () => {
+    const src = 'http://localhost/test.png';
+
+    setBaseComponentSetup(component);
+    fixture.detectChanges();
+
+    component.layer.setUrl = jest.fn();
+    component.src = src;
+
+    fixture.detectChanges();
+
+    expect(component.layer.setUrl).toHaveBeenCalled();
+    expect(component.layer.setUrl).toHaveBeenCalledWith(src);
+  });
 });
+
+function setBaseComponentSetup(component: ImageOverlayComponent) {
+  component.map = new Map(document.createElement('div'), {
+    crs: CRS.Simple
+  });
+  component.src = url;
+  component.height = height;
+  component.width = width;
+}
--- lib/src/image-overlay/image-overlay.component.ts
+++ lib/src/image-overlay/image-overlay.component.ts
@@ -1,4 +1,4 @@
-import {Component, forwardRef, Input, OnDestroy} from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
 import {imageOverlay, ImageOverlay, LatLngBounds, latLngBounds, LayerGroup, Map} from 'leaflet';
 import {BaseLayer} from '../core/base-layer';
 
